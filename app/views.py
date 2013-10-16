5ad0695acd6eab41c8b942131f1e1e1a22a0be27
punkracy <punkracy@gmail.com>
2013-10-16 18:45:02 +0000

    add bootstrap

--- app/views.py
+++ app/views.py
@@ -1,5 +1,15 @@
+from flask import render_template, send_from_directory
 from app import app
+import os
+@app.route('/favicon.ico')
+def favicon():
+  return send_from_directory(os.path.join(app.root_path, 'static'), 'img/favicon.ico')
+
+@app.errorhandler(404)
+def page_not_found(e):
+  return render_template('404.html'), 404
+
 @app.route('/')
 @app.route('/index')
 def index():
-  return "Hello, World!"
+  return render_template('index.html')
